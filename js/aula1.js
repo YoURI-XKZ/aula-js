96e9ef378f2e175be5906728bf813cb325a90bf8
YoURI <yurioliveiraferndes@gmail.com>
2024-09-18 19:29:26 -0300

    string javascript

--- js/aula1.js
+++ js/aula1.js
@@ -1,4 +1,42 @@
 console.log("Hello World");
 
-alert("Soso");
-document.write("eu amo vc soso");
+alert("suicidio");
+document.write("HELLO WORLD");
+
+//* aspas no javascript
+
+console.log ('javascript');
+console.log ("python");
+console.log (`html`);
+console.log (`eu 'te' "odeio"`);
+console.log ('podemos usar "aspas duplas" numa string');
+
+
+
+document.write ("yuyu");
+nome = "Yuri";
+var idade = 17;
+let peso = 67;
+const estadopsicologicoestavel = false;
+
+console.log ("sua idade é, ",idade);
+
+let vazia;
+
+
+document.write ("qual seu filme preferido?")
+var filme = "Fight Club"
+document.write ("- meu filme favorito é ", filme)
+document.write ("quantos anos vc teria daqui 50 anos?")
+var idade = 17
+document.write ("- eu teria",idade + 50)
+
+
+
+
+
+////*penisenorme
+let frase1 = "e eu sinto";
+let frase2 = "o shadow no meu cu";
+
+document.write 
---
+++ js/aula2.js
@@ -0,0 +1,36 @@
+let nome = "clube da luta"
+
+
+document.write ("<h3>Selecionado: ", nome[6],"</h3>");
+
+
+//* seleção apos o limite
+console.log(" a 14ª letra é: ", nome [14])
+//^ abaixo do minimo
+console.log(" a -2 letra é: ", nome [-2])
+
+
+
+
+
+
+//* funções string---------------------------------------
+
+console.log("o indice 8 é: ",nome.charAt(8));
+
+let frase = "e eu sinto Shadow no meu cu, larissa não quero teus beijus";
+console.log('O indice de "Shadow" É: ',frase.indexOf(Shadow));
+
+
+let frase2 = "só me apaixono por ouriços pretos ou com azuils nos pelos";
+
+console.log =('o indice de "ouriços" é: ', frase2.indexOf("aranha"))
+
+//* função replace - substitui um string dentro de texto
+
+
+
+    console.log ("===============replace==================");
+
+    console.log ("subistituindo ouriços", frase2.replace("ouriços","furrys"));
+ 
