6a77b1641af8e4e5761ba7f300761f02695f59d1
YoURI <yurioliveiraferndes@gmail.com>
2024-09-23 22:14:31 -0300

    jokepo 1

---
+++ js/jogo.js
@@ -0,0 +1,19 @@
+let playerscre = 0
+let machinescr = 0
+
+const playerscredisplay = document.getElementById("playerscore")
+const machinescrdisplay = document.getElementById("machinescore")
+const resultdisplay = document.getElementById("result")
+const body = documen.body;
+
+btnrock.addeventlistener("click", function(){
+    playround("pedra");
+})
+
+btnpaper.addeventlistener("click", function(){
+    playround("pedra");
+})
+
+btnscissor.addeventlistener("click", function(){
+    playround("pedra");
+})
