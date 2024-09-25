0f8d49817e3be35a3af3809f53b9923763887de8
YoURI <yurioliveiraferndes@gmail.com>
2024-09-24 21:25:21 -0300

    bloodbet

--- js/jogo.js
+++ js/jogo.js
@@ -4,16 +4,39 @@ let machinescr = 0
 const playerscredisplay = document.getElementById("playerscore")
 const machinescrdisplay = document.getElementById("machinescore")
 const resultdisplay = document.getElementById("result")
-const body = documen.body;
+const body = document.body;
 
-btnrock.addeventlistener("click", function(){
+const btnrock = document.getElementById ("rock")
+const btnpaper = document.getElementById ("paper")
+const btnscissor = document.getElementById ("scissor")
+
+//-------------------------------------------------------------
+btnrock.addEventListener("click", function(){
     playround("pedra");
 })
 
-btnpaper.addeventlistener("click", function(){
+btnpaper.addEventListener("click", function(){
     playround("pedra");
 })
 
-btnscissor.addeventlistener("click", function(){
+btnscissor.addEventListener("click", function(){
     playround("pedra");
 })
+
+function getmachinechoice(){
+    const choices = ["pedra","papel","tesoura"];
+    const randomindex = Math.floor(Math.random()* choices.length);
+    return choices [randomindex];
+
+    
+
+}
+///^ random * possibilidades)  ( lengh tamanho na da variavel)
+
+function playround(playerchoice){
+    //* machinechoice recebe retorno da função getmachinechoice, escolhe 
+    const machinechoice = getmachinechoice();
+    let result;
+}
+
+
